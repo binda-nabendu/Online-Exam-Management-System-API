421dc3e96a683f5f4ac4a6e00dad89cbcf8d211b
BindaNabendu <bindanabendu@gmail.com>
2021-12-27 23:08:28 +0600

    Project create started

---
+++ src/test/java/com/oems/home/ExamManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oems.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
