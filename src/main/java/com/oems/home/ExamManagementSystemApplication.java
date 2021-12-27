d3945efb6de8a59573f597e0c0e1e9d60d290fb1
BindaNabendu <bindanabendu@gmail.com>
2021-12-27 23:08:28 +0600

    Project create started

---
+++ src/main/java/com/oems/home/ExamManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.oems.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamManagementSystemApplication.class, args);
+	}
+
+}
