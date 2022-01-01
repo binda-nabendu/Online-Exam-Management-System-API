a9f47ea9542c0c6e81bdb4442f5f923b6280a83f
BindaNabendu <bindanabendu@gmail.com>
2022-01-01 22:28:52 +0600

    fix some problem

--- src/main/java/com/oems/home/controller/AdminController.java
+++ src/main/java/com/oems/home/controller/AdminController.java
@@ -10,65 +10,60 @@ import javax.ws.rs.Path;
 @RestController
 public class AdminController {
 
-    @GetMapping
-    @RequestMapping("/admin-board/{user-id}")
+    @GetMapping("/admin-board/{user-id}")
     public String adminBoardManager(@PathVariable("user-id") String user){
         return "This return admin Dashboard";
     }
 
-    @PostMapping
-    @RequestMapping("/teachers/add-teacher/{teacher-details}")
+    @PostMapping("/teachers/add-teacher/{teacher-details}")
     public String addTeacher(@PathVariable("teacher-details") Teacher teacher){
         return "This return admin Dashboard";
     }
 
     //-----------For teacher approve-------------
-    @GetMapping
-    @RequestMapping("/teachers/approve-teachers/list")
+    @GetMapping("/teachers/approve-teachers/list")
     public String approveTeacher(){
         return "All teacher list who are request for approve";
     }
 
-    @PostMapping
-    @RequestMapping("/teachers/approve-teachers/approve/{teacher-id}")
+    @PostMapping("/teachers/approve-teachers/approve/{teacher-id}")
     public String approveTeacher(@PathVariable("teacher-id") String tId){
         return "Teacher who will be approved by admin now";
     }
 
-    @PostMapping
-    @RequestMapping("/teachers/approve-teachers/delete/{teacher-id}")
+    @PostMapping("/teachers/approve-teachers/delete/{teacher-id}")
     public String deleteTeacher(@PathVariable("teacher-id") String tId){
         return "Teacher who will be deleted by admin now";
     }
 
     //-----------For student approve-------------
-    @PostMapping
-    @RequestMapping("/student/add-student/{student-details}")
+    @PostMapping("/student/add-student/{student-details}")
     public String addTeacher(@PathVariable("student-details") Student student){
         return "Student added successful";
     }
 
 
-    @GetMapping
-    @RequestMapping("/student/approve-student/list")
+    @GetMapping("/student/approve-student/list")
     public String approveStudent(){
         return "All student list who are request for approve";
     }
 
-    @PostMapping
-    @RequestMapping("/student/approve-student/approve/{student-id}")
+    @PostMapping("/student/approve-student/approve/{student-id}")
     public String approveStudent(@PathVariable("student-id") String sId){
         return "Student who will be approved by admin now";
     }
 
-    @PostMapping
-    @RequestMapping("/student/approve-student/delete/{student-id}")
+    @PostMapping("/student/approve-student/delete/{student-id}")
     public String deleteStudent(@PathVariable("student-id") String sId){
         return "Student who will not be approve instead of delete them";
     }
 
-    @PostMapping
-    @RequestMapping("/courses/add-courses/{course-details}")
+    @PostMapping("/courses/add-department/{department-details}")
+    public String addDepartment(@PathVariable("department-details") CourseDetails department){
+        return "department Added Successful";
+    }
+
+    @PostMapping("/courses/add-courses/{course-details}")
     public String addCourse(@PathVariable("course-details") CourseDetails details){
         return "Course Added Successful";
     }
--- src/main/java/com/oems/home/controller/StudentController.java
+++ src/main/java/com/oems/home/controller/StudentController.java
@@ -4,40 +4,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
-    @GetMapping
-    @RequestMapping("student-board/{id}")
+    @GetMapping("student-board/{id}")
     public String studentBoardManager(@PathVariable("id") String id){
 
         return "Student board manager";
     }
 
-    @GetMapping
-    @RequestMapping("/departmental-course/my-courses/{studentId}")
+    @GetMapping("/departmental-course/my-courses/{studentId}")
     public String studentCurrentSubject(@PathVariable("studentId") String id){
         return "All Courses";
     }
-    @GetMapping
-    @RequestMapping("/departmental-course/{deptId}")
+    @GetMapping("/departmental-course/{deptId}")
     public String allCourseOfThatDepartment(@PathVariable("deptId") String deptId){
         return "All course of that department";
     }
-    @GetMapping
-    @RequestMapping("/departmental-course/completed/{studentId}")
+    @GetMapping("/departmental-course/completed/{studentId}")
     public String allCompletedCourseOfThatStudent(@PathVariable("studentId") String id){
         return "All completed course of that Student";
     }
-    @GetMapping
-    @RequestMapping("/exams/upcoming/{studentId}")
+    @GetMapping("/exams/upcoming/{studentId}")
     public String upComingExamStudent(@PathVariable("studentId") String id){
         return "List of upcoming exam";
     }
-    @GetMapping
-    @RequestMapping("/exams/previous/{studentId}")
+    @GetMapping("/exams/previous/{studentId}")
     public String previusExamStudent(@PathVariable("studentId") String id){
         return "List of previous exam up to 2 semester";
     }
-    @PostMapping
-    @RequestMapping("/exams/send-review/{studentId}/{examId}")
+    @PostMapping("/exams/send-review/{studentId}/{examId}")
     public String RequestReview(@PathVariable("studentId")String id ,@PathVariable("examId") int examId){
         return "Review Request Sand..";
     }
