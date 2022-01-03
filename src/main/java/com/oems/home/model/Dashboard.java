bbee69286d9e08bd7260adea7244c58405f98823
BindaNabendu <bindanabendu@gmail.com>
2022-01-03 23:20:12 +0600

    Implement admin deshboard method

--- src/main/java/com/oems/home/model/Dashboard.java
+++ src/main/java/com/oems/home/model/Dashboard.java
@@ -7,4 +7,43 @@ public class Dashboard {
     private int totalExamTakenByThatTeacher;
     private int totalUpComingExam;
 
+    public int getTotalTeachers() {
+        return totalTeachers;
+    }
+
+    public void setTotalTeachers(int totalTeachers) {
+        this.totalTeachers = totalTeachers;
+    }
+
+    public int getTotalStudents() {
+        return totalStudents;
+    }
+
+    public void setTotalStudents(int totalStudents) {
+        this.totalStudents = totalStudents;
+    }
+
+    public int getTotalDepartments() {
+        return totalDepartments;
+    }
+
+    public void setTotalDepartments(int totalDepartments) {
+        this.totalDepartments = totalDepartments;
+    }
+
+    public int getTotalExamTakenByThatTeacher() {
+        return totalExamTakenByThatTeacher;
+    }
+
+    public void setTotalExamTakenByThatTeacher(int totalExamTakenByThatTeacher) {
+        this.totalExamTakenByThatTeacher = totalExamTakenByThatTeacher;
+    }
+
+    public int getTotalUpComingExam() {
+        return totalUpComingExam;
+    }
+
+    public void setTotalUpComingExam(int totalUpComingExam) {
+        this.totalUpComingExam = totalUpComingExam;
+    }
 }
