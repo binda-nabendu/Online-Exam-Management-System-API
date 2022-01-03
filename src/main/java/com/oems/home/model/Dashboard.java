7a7b477e3760551c5ea00494db889c0149428fe6
BindaNabendu <bindanabendu@gmail.com>
2022-01-03 16:12:18 +0600

    implemented all aprove student related method and add dashboard object

---
+++ src/main/java/com/oems/home/model/Dashboard.java
@@ -0,0 +1,10 @@
+package com.oems.home.model;
+
+public class Dashboard {
+    private int totalTeachers;
+    private int totalStudents;
+    private int totalDepartments;
+    private int totalExamTakenByThatTeacher;
+    private int totalUpComingExam;
+
+}
