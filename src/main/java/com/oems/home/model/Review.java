0d6ae347b2cd3053525e5b781ce80ee601b72dbf
BindaNabendu <bindanabendu@gmail.com>
2022-01-09 02:49:34 +0600

    Add so many controller method

---
+++ src/main/java/com/oems/home/model/Review.java
@@ -0,0 +1,49 @@
+package com.oems.home.model;
+
+public class Review {
+    private String courseCode;
+    private int examId;
+    private String stdId;
+    private String stdName;
+    private double gotTotalMarks;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public int getExamId() {
+        return examId;
+    }
+
+    public void setExamId(int examId) {
+        this.examId = examId;
+    }
+
+    public String getStdId() {
+        return stdId;
+    }
+
+    public void setStdId(String stdId) {
+        this.stdId = stdId;
+    }
+
+    public String getStdName() {
+        return stdName;
+    }
+
+    public void setStdName(String stdName) {
+        this.stdName = stdName;
+    }
+
+    public double getGotTotalMarks() {
+        return gotTotalMarks;
+    }
+
+    public void setGotTotalMarks(double gotTotalMarks) {
+        this.gotTotalMarks = gotTotalMarks;
+    }
+}
---
+++ src/main/java/com/oems/home/model/UserVerificationModel.java
@@ -0,0 +1,40 @@
+package com.oems.home.model;
+
+public class UserVerificationModel {
+    private String nid;
+    private String email;
+    private String password;
+    private String role;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNid() {
+        return nid;
+    }
+
+    public void setNid(String nid) {
+        this.nid = nid;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
