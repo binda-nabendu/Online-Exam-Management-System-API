3f825feefedbae4ec898ba42d1c21b7d730d40da
BindaNabendu <bindanabendu@gmail.com>
2022-01-10 18:05:56 +0600

    upcomingExamForStudent, prevExamForStudent implementation complete and modify in database,questionsummary,student and teacher controller

--- src/main/java/com/oems/home/controller/StudentController.java
+++ src/main/java/com/oems/home/controller/StudentController.java
@@ -6,6 +6,7 @@ import com.oems.home.model.CourseDetails;
 import com.oems.home.model.Dashboard;
 import com.oems.home.model.QuestionPaper;
 
+import com.oems.home.model.QuestionSummery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,11 +42,11 @@ public class StudentController {
         return studentDao.completedCoursesByStudent(stdId);
     }
     @GetMapping("/exams/upcoming/{studentId}")
-    public List<CourseDetails> upComingExamStudent(@PathVariable("studentId") String stdId){
+    public List<QuestionSummery> upComingExamStudent(@PathVariable("studentId") String stdId){
     	return studentDao.upcomingExamForStudent(stdId);
     }
     @GetMapping("/exams/previous/{studentId}")
-    public List<CourseDetails> previusExamStudent(@PathVariable("studentId") String stdId){
+    public List<QuestionSummery> previousExamStudent(@PathVariable("studentId") String stdId){
     	return studentDao.prevExamForStudent(stdId);
     }
     @PostMapping("/exams/send-review/{studentId}/{examId}")
--- src/main/java/com/oems/home/model/QuestionSummery.java
+++ src/main/java/com/oems/home/model/QuestionSummery.java
@@ -2,12 +2,13 @@ package com.oems.home.model;
 
 public class QuestionSummery {
 
+    private int examId;
 	private String courseCode;
 	private String teacherId;
 	private Double percentageValue;
 	private String startingDateTime;
+	private String endingDateTime;
     private double total;
-    private int examId;
 	public QuestionSummery() {
 		super();
 	}
@@ -19,6 +20,12 @@ public class QuestionSummery {
 		this.percentageValue = percentageValue;
 		this.startingDateTime = startingDateTime;
 		this.total = total;
+		this.examId = examId;
+	}
+	public int getExamId() {
+		return examId;
+	}
+	public void setExamId(int examId) {
 		this.examId = examId;
 	}
 	public String getCourseCode() {
@@ -45,17 +52,20 @@ public class QuestionSummery {
 	public void setStartingDateTime(String startingDateTime) {
 		this.startingDateTime = startingDateTime;
 	}
+
+	public String getEndingDateTime() {
+		return endingDateTime;
+	}
+
+	public void setEndingDateTime(String endingDateTime) {
+		this.endingDateTime = endingDateTime;
+	}
+
 	public double getTotal() {
 		return total;
 	}
 	public void setTotal(double total) {
 		this.total = total;
-	}
-	public int getExamId() {
-		return examId;
-	}
-	public void setExamId(int examId) {
-		this.examId = examId;
 	}
     
 	
