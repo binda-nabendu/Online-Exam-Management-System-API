7bc491f6a06c245771f1e3b08fb9b3f54f73a01b
BindaNabendu <bindanabendu@gmail.com>
2022-01-12 16:01:01 +0600

    All contorller current method will check and modify if necessery

--- src/main/java/com/oems/home/dao/AdminJdbcDao.java
+++ src/main/java/com/oems/home/dao/AdminJdbcDao.java
@@ -22,7 +22,7 @@ public class AdminJdbcDao{
         String q1= "select COUNT(*) from teacher";
         String q2= "select COUNT(*) from student";
         String q3= "select COUNT(*) from department";
-        String q4 = "select COUNT(*) from examPaper where teacherId="+id;
+        String q4= "select COUNT(*) from examPaper where teacherId="+id;
 
         Dashboard dashboard = new Dashboard();
         dashboard.setCard1(Optional.ofNullable(jdbcTemplate.queryForObject(q1, Integer.class)).orElse(-1));
@@ -63,39 +63,61 @@ public class AdminJdbcDao{
 
     public List<RequestCourse> listOfRequestedCourses() {
         String q1="select * from student s,requestcourse r,courses c" +
-                " where s.stdId=r.stdId and r.courseCode=c.courseCode";
+                " where s.stdId=r.stdId and r.courseCode=c.courseCode and r.deptId=c.deptId";
         return jdbcTemplate.query(q1,(rs, rowNumber)->{
             RequestCourse requestedCourse = new RequestCourse();
+            requestedCourse.setStdId(rs.getString("stdId"));
+            requestedCourse.setDeptId(rs.getString("deptId"));
+            requestedCourse.setBatch(rs.getInt("batch"));
+            requestedCourse.setSemester(rs.getInt("semester"));
             requestedCourse.setCourseCode(rs.getString("courseCode"));
+            requestedCourse.setCourseName(rs.getString("courseName"));
+            requestedCourse.setTeacherId(rs.getString("teacherId"));
+            requestedCourse.setCourseCurrSession(rs.getString("courseCurrSession"));
             return requestedCourse;
         });
     }
 
-    public void approveCoursesForStudent(List<RequestCourse> list) {
-        for(RequestCourse requestedCourse : list){
-            String queryForRemoveFormRequestedCourseTable = " delete from requestcourse where stdId="
-                                                            +requestedCourse.getStdId()+" and courseCode="
-                                                            +requestedCourse.getCourseCode();
-            String queryForAddSub = " insert into result " +
-                                    "(stdId, courseCode,courseSession,semester) " +
-                                    "values (?,?,?,?)";
-            jdbcTemplate.update(queryForRemoveFormRequestedCourseTable);
-            jdbcTemplate.update(queryForAddSub, requestedCourse.getStdId(),requestedCourse.getCourseCode(),requestedCourse.getCourseCurrSession(),requestedCourse.getSemester());
-        }
+    public void approveCoursesForStudent(RequestCourse requestedCourse, boolean isDelete) {
+
+        String queryForDelFrmRequestCourse = "delete from requestCourse where stdId=? " +
+                "and courseCode=? and deptId=?";
+
+        String queryForAddSub = " insert into result " +
+                "(stdId, courseCode,deptId,courseSession,semester) " +
+                "values (?,?,?,?,?)";
+
+        jdbcTemplate.update(queryForDelFrmRequestCourse, requestedCourse.getStdId(),
+                requestedCourse.getCourseCode(), requestedCourse.getDeptId());
+
+        if(isDelete) return;
+
+        jdbcTemplate.update(queryForAddSub, requestedCourse.getStdId(),
+                requestedCourse.getCourseCode(), requestedCourse.getDeptId(), requestedCourse.getCourseCurrSession(),
+                requestedCourse.getSemester());
+
+
     }
 
     public void addACourses(CourseDetails details) {
-        if(details.getTeacherId().length()<2){
-            details.setTeacherId("Not assigned");
-        }
-        if(details.getCourseSessions()<0){
+        if (details.getCourseSessions() < 0) {
             details.setCourseSessions(-9999);
         }
-        String queryForAddCourse = "Insert into courses " +
-                " (courseCode, deptId, courseName,teacherId, courseCurrSession)  " +
-                "values(?,?,?,?,?)";
-        jdbcTemplate.update(queryForAddCourse,details.getCourseCode(),details.getDeptId(),details.getCourseName(),details.getTeacherId(),details.getCourseSessions());
+        if(details.getTeacherId().length()<2) {
 
+
+            String queryForAddCourse = "Insert into courses " +
+                    " (courseCode, deptId, courseName, courseCurrSession)  " +
+                    "values(?,?,?,?)";
+            jdbcTemplate.update(queryForAddCourse, details.getCourseCode(), details.getDeptId(),
+                    details.getCourseName(), details.getCourseSessions());
+        }else{
+            String queryForAddCourse = "Insert into courses " +
+                    " (courseCode, deptId, courseName,teacherId, courseCurrSession)  " +
+                    "values(?,?,?,?,?)";
+            jdbcTemplate.update(queryForAddCourse, details.getCourseCode(), details.getDeptId(),
+                    details.getCourseName(), details.getTeacherId(), details.getCourseSessions());
+        }
     }
 
     public void addADepartment(Department department) {
