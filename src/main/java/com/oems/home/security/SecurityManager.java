257f20e01061f6cc4d44f5b669f5d11814b76dbe
BindaNabendu <bindanabendu@gmail.com>
2022-01-15 01:40:35 +0600

    security looks working but not sure

--- src/main/java/com/oems/home/security/SecurityManager.java
+++ src/main/java/com/oems/home/security/SecurityManager.java
@@ -1,43 +1,71 @@
 package com.oems.home.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityManager extends WebSecurityConfigurerAdapter {
 
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public void configAuthentication(AuthenticationManagerBuilder authBuilder) throws Exception {
+//        authBuilder.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .usersByUsernameQuery("select nid, password, adminApproval from baseUser where nid=?")
+//                .authoritiesByUsernameQuery("select nid, role from baseUser where nid=?")
+//                ;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.authorizeRequests()
+//                .antMatchers("/admin/**").access("hasAuthority('ADMIN')")
+//                .antMatchers("/teacher/**").access("hasAuthority('TEACHER') OR hasAuthority('ADMIN')")
+//                .antMatchers("/student/**").access("hasAuthority('STUDENT') OR hasAuthority('TEACHER') OR hasAuthority('ADMIN')")
+//                .antMatchers("/").permitAll()
+//                .and()
+//                .formLogin().permitAll()
+//                .and()
+//                .logout().permitAll();
+//    }
     @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder authBuilder) throws Exception {
-        authBuilder.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(new BCryptPasswordEncoder())
-                .usersByUsernameQuery("select nid, password, adminApproval from baseUser where nid=?")
-                .authoritiesByUsernameQuery("select nid, role from baseUser where nid=?")
-                ;
-    }
+    private MyUserDetailsService myUserDetailsService;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests()
-                .antMatchers("/admin/**").access("hasAuthority('ADMIN')")
-                .antMatchers("/teacher/**").access("hasAuthority('TEACHER') OR hasAuthority('ADMIN')")
-                .antMatchers("/student/**").access("hasAuthority('STUDENT') OR hasAuthority('TEACHER') OR hasAuthority('ADMIN')")
-                .antMatchers("/").permitAll()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().permitAll();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
     }
 
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
