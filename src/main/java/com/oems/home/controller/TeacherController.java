382e3e794c45b42829723f11f05235869d59f77e
tawhidh <tawhiidhassan@gmail.com>
2022-01-16 00:15:35 -0800

    implement some method in admin controller and teacher controller with necessery jdbc method

--- src/main/java/com/oems/home/controller/TeacherController.java
+++ src/main/java/com/oems/home/controller/TeacherController.java
@@ -52,13 +52,26 @@ public class TeacherController {
     }
 
     @GetMapping("/teacher/all-questions/{teacher-id}")
-    public List<QuestionSummery> getAllQuestionThatTeacherMade(@PathVariable("teacher-id") String tid){
-        return examDao.returnAllQuestionAccordingToTeacher(tid);
+    public List<QuestionSummery> getAllQuestionThatTeacherMade(@PathVariable("teacher-id") String tId){
+        return examDao.returnAllQuestionAccordingToTeacher(tId);
     }
+    //t
     @GetMapping("/teacher/all-pending-result/{teacher-id}")
-    public String ResultTeacherHaveToPublish(@PathVariable("teacher-id") String tid){
-        return "Here all of your subject result you can see"+tid;
+    public List<QuestionSummery> allPendingResult(@PathVariable("teacher-id") String tId){
+        return teacherDao.listOfAllPendingResult(tId);
     }
+    @GetMapping("/teacher/all-pending-result/student-list{exam-id}")
+    public List<Student> allPendingResultStdList(@PathVariable("exam-id") int examId){
+        return teacherDao.listOfAllPendingResultStdList(examId);
+    }
+    @GetMapping("/teacher/{course-code}") //????
+    public List<Student> readyStudentForCgpaOfThatCourse(@PathVariable("course-code") String courseCode){
+        return teacherDao.listOfReadyStudentForCgpaOfThatCourse(courseCode);
+    }
+    
+    
+    
+    
     @GetMapping("/teacher/receive-review/{teacher-id}")
     public List<Review> ReviewList(@PathVariable("teacher-id") String tid){
         return teacherDao.studentExamPaperReviewList(tid);
